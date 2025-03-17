725d90cc53aea1719208551ae5894fccf00f0ea8
oscardcd <oscardcd17@gmail.com>
2025-03-16 19:18:38 -0500

    📝 (docs): Remove unused 404.html file 🔧 (docs): Add meta tags for Permissions-Policy and Content-Security-Policy to improve security in HTML files 🔧 (server): Update asset sizes and hashes in angular-app-manifest.mjs for consistency 🔧 (server): Add meta tags for Permissions-Policy and Content-Security-Policy in server HTML files 🔧 (src): Add meta tags for Permissions-Policy and Content-Security-Policy in index.html for security enhancements

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5107, hash: '673dd2c3633b4d52462addcf22bfc5fffae917556e17db1d46a4436687964b99', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1065, hash: '0c0e424b3ddd183f015606bbea391839158b77c8142a5ad9bf2906bc860158a1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5259, hash: '8b987329eac2ce7c099b93fb39767609154e2323519bc4d574bf90fe3c5b7690', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1217, hash: '16601fcff7449167d3e482ecec6b776d251b22aa1f53160f4d46d73a937be21f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-M45EAINT.css': {size: 231770, hash: '/dVeW4/Z9Pg', text: () => import('./assets-chunks/styles-M45EAINT_css.mjs').then(m => m.default)}
   },
 };
