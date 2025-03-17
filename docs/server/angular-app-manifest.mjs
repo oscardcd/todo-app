f1f0063974ff9813e1d339481c2efd470174be7a
oscardcd <oscardcd17@gmail.com>
2025-03-16 23:38:45 -0500

    add needed files

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 980, hash: 'cbb55d2c561ba72590a72311f10cf353b511cf3c14334fcdf5f62146612b284f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1209, hash: '46011c74b5304ca9b5bbde37593020ed8a8e56531646b9b2fbbc4cff9d5cb775', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-32DGMBZE.css': {size: 159, hash: 'yw15ZFrMsZ4', text: () => import('./assets-chunks/styles-32DGMBZE_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 935, hash: '2c8e06c78ed7261a2305175ff507474d083c63a270aefd05f3fe74f281a1caaa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1173, hash: '1254a8ad20ca2d598c72a6cc91767d8b4f49060fcc3a5942f2c643b357671d4b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles.css': {size: 159, hash: 'yw15ZFrMsZ4', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
